3442202c98c065d527c1443aa9c8d071de51b785
nurealammiaji <nurealam.miaji@gmail.com>
2023-07-19 10:12:14 +0600

    blog creation and data parsing function added

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,9 +1,29 @@
 import './Blog.css';
 
 const Blog = (props) => {
+
+    const {author, author_image, image, title, read_time, publish_date} = props.blog;
+    
     return (
         <div>
-            <h1>{props.blogs.length} </h1>
+            <div className='Blog'>
+                <img src={image} alt="" />
+                <div className='blog-details'>
+                    <div className='blog-author-details'>
+                        <div className='author-image'>
+                            <img src={author_image} alt="" />
+                        </div>
+                        <div className='author-info'>
+                            <h4>{author}</h4>
+                            <p>{publish_date}</p>
+                        </div>
+                    </div>
+                    <div>
+                        <p>{read_time}</p>
+                    </div>
+                </div>
+                <h2>{title}</h2>
+            </div>
         </div>
     );
 };
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -15,7 +15,9 @@ const Layout = () => {
     return (
         <div className='layout'>
             <div className='blog-area'>
-                <Blog blogs={blogs}></Blog>
+                {
+                    blogs.map(blog => <Blog key={blog.id} blog={blog}></Blog>)
+                }
             </div>
             <div className='blog-sidebar'>
             </div>
