5052b31487fa7d3bd283ba64ab78793f653dbab4
nurealammiaji <nurealam.miaji@gmail.com>
2023-07-19 11:00:40 +0600

    blog style modified

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -18,8 +18,13 @@ const Blog = (props) => {
                             <p>{publish_date}</p>
                         </div>
                     </div>
-                    <div>
-                        <p>{read_time}</p>
+                    <div className='reading-details'>
+                        <div>
+                            <p>{read_time}</p>
+                        </div>
+                        <div className='bookmark-btn'>
+                            <button><svg id="i-bookmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path d="M6 2 L26 2 26 30 16 20 6 30 Z" /></svg></button>
+                        </div>
                     </div>
                 </div>
                 <h2>{title}</h2>
